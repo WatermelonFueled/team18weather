0a93025e678c5f2068486f2d43858e28d52f124b
Diana Godoy <dgodoysm@uwo.ca>
2015-03-05 22:53:56 -0500

    Maven initial build automation setup for Weather App

---
+++ WeatherApp/src/main/java/ca/uwo/csd/cs2212/team18/AppMain.java
@@ -0,0 +1,13 @@
+package ca.uwo.csd.cs2212.team18;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AppMain {
+	static Logger logger = LogManager.getLogger(AppMain.class.getName());	
+	public static void main (String [] args) {
+		logger.trace("Entering main");
+		logger.warn("Hello Maven/log4j World");
+		logger.info("This is Team 18's Weather App");
+		logger.trace("Exiting main");
+	}
+}

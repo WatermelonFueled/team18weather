41578e6249a9292add9d339768fb9eacc4873c33
David Park <dpark9@uwo.ca>
2015-03-07 13:21:30 -0500

    changed AppMain.java to perform basic initializations

--- src/main/java/ca/uwo/csd/cs2212/team18/AppMain.java
+++ src/main/java/ca/uwo/csd/cs2212/team18/AppMain.java
@@ -3,19 +3,30 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AppMain {
-	static Logger logger = LogManager.getLogger(AppMain.class.getName());	
-	public static void main (String [] args) {
-		logger.trace("Entering main");
-		logger.warn("Hello Maven/log4j World");
-		logger.info("This is Team 18's Weather App");
-		logger.trace("Exiting main");
-        
-        SwingUtilities.invokeLater(new Runnable() {
-        @Override
-            public void run() {
-                SelectionPage sp = new SelectionPage();
-                sp.setVisible(true);
-            }
-        });
+
+    
+    SelectionPage selectionPage = new SelectionPage();
+    DataRequester dataRequester = new dataRequester();
+    LocalWeatherData localData = new LocalWeatherData();
+
+    
+    static Logger logger = LogManager.getLogger(AppMain.class.getName());	
+    public static void main (String [] args) {
+            logger.info("This is Team 18's Weather App");
+
+            // initialize objects
+            SelectionPage selectionPage = new SelectionPage();
+            DataRequester dataRequester = new dataRequester();
+            LocalWeatherData localData = new LocalWeatherData();
+            //search
+
+            //request data
+
+            //send to local view and display
+
+
+
     }
+
+
 }
